72a61e8bf5a517d122a02610cf42a6de97be396f
rhidalgo <rhidalgo4@mail.greenriver.edu>
2020-01-15 21:55:39 -0800

    Created Arrays

---
+++ python/arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Demonstrates using Arrays in Python
+from array import *
+
+
+my_array = array('i', [1,2,3,4,5])
+print(my_array[0])
+print(my_array[2])
+print(my_array[-1])
+print(my_array)
+
+
