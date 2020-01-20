7715944374f9886bccd6af8a4ca122d26d311c3b
rhidalgo <rhidalgo4@mail.greenriver.edu>
2020-01-19 22:17:43 -0800

    Created a forloop

---
+++ python/forloop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Example for loop
+
+def ForRange():
+	for myNum in range(5):
+		print(myNum)
+
+def ForList():
+	for myNum in [0,1,2,3,4]:
+		print(myNum)
+
+ForRange()
+ForList()
