8822c658a5a32b89aea6f3c7eba22a1f3942b361
rhidalgo <rhidalgo4@mail.greenriver.edu>
2020-01-23 20:22:03 -0800

    Function with default values

--- python/functions.py
+++ python/functions.py
@@ -8,11 +8,26 @@ def func1():
 def addNums(val1, val2):
 	print(val1 + val2)
 
+# Function with a default value
+def power(num, power = 2):
+	result = 1
+	for i in range(power):
+		result = result * num
+	print(result)
 
 # call functions
 #func1()
 #print(func1())
 #print(func1)
 
-addNums(7, 11)
-addNums(val2 = 11, val1 = 7)
+#addNums(7, 11)
+#addNums(val2 = 11, val1 = 7)
+
+power(5)
+power(5,2)
+power(5,3)
+
+
+
+
+
