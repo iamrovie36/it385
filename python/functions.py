8ba7fd835c69f7e1a0c449a345574ec3134b1f0a
rhidalgo <rhidalgo4@mail.greenriver.edu>
2020-01-23 20:15:01 -0800

    Function arguments

--- python/functions.py
+++ python/functions.py
@@ -4,7 +4,15 @@
 def func1():
 	print("Lets get funky")
 
+# Function that takes an argument
+def addNums(val1, val2):
+	print(val1 + val2)
+
+
 # call functions
-func1()
-print(func1())
-print(func1)
+#func1()
+#print(func1())
+#print(func1)
+
+addNums(7, 11)
+addNums(val2 = 11, val1 = 7)
