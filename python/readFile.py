3498e528526f53f60e38f8fbb757dd831c69d3e8
rhidalgo <rhidalgo4@mail.greenriver.edu>
2020-01-25 21:03:11 -0800

    Read a File

--- python/readFile.py
+++ python/readFile.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 # Script that reads a file
+
+# easy reading of file
+myFile = open("testfile.txt", "r")
+print(myFile.read(5))
+print(myFile.read())
+
+# reading file line by line
+myFile = open("testfile.txt", "r")
+for line in myFile:
+	if line.startswith("a"):
+		print(line)
+myFile.close()
+
+# Read file usoing a WITH statement
+with open("testfile.txt", "r") as myFile:
+	print(myFile.read())
+
